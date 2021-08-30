1213982cf50cf23034e9f5bd3fa6ad27632db17f
Neil Italia <neilitalia1998@gmail.com>
2021-08-30 12:57:20 -0500

    :label: Create Persons schema

---
+++ models/persons.js
@@ -0,0 +1,10 @@
+const { Schema } = require('mongoose')
+
+const Person = new Schema(
+  {
+    name: { type: String, required: true },
+    tickets: [{ type: Schema.Types.ObjectId, ref: 'tickets' }]
+  },
+  { timestamps: true }
+)
+module.exports = Post
