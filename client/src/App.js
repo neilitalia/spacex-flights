64ce02f88d4adcce102b1086d45a29cec31f8d92
Neil Italia <neilitalia1998@gmail.com>
2021-08-30 23:17:47 -0500

    :sparkles: Create Header

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
+import Header from './components/Header'
 import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Header />
     </div>
   )
 }
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header className="Header">
+      <h1>One giant leap for mankind.</h1>
+      <p>Our next step to becoming multi-planet species: Go where you want to go. Explore the known and unknown. Find cities terraformed to your taste. A new variety of civilizations await your arrival. </p>
+    </header>
+  )
+}
+
+export default Header
