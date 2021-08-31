b7f1f9cd3144aec006dbe512c8006dc1744bed89
Neil Italia <neilitalia1998@gmail.com>
2021-08-31 14:46:19 -0500

    :construction: Draft EarliestFlight component

---
+++ client/src/components/EarliestFlight.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './EarliestFlight.css'
+
+const EarliestFlight = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  )
+}
+
+export default EarliestFlight
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,6 +6,8 @@ const Header = () => {
     <header className="Header">
       <h1>One giant leap for mankind.</h1>
       <p>Our next step to becoming multi-planet species: Go where you want to go. Explore the known and unknown. Find cities terraformed to your taste. A new variety of civilizations await your arrival. </p>
+      <button className="earliest-flight">Join Earliest Flight</button>
+      <button className="all-flights">Explore Destinations</button>
     </header>
   )
 }
