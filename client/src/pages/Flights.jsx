4305428638c448909f4529e1144d0b9ba33e0fdc
Neil Italia <neilitalia1998@gmail.com>
2021-08-31 23:41:16 -0500

    :sparkles: Add state to search inputs

--- client/src/pages/Flights.jsx
+++ client/src/pages/Flights.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Flights.css'
 import Search from '../icons/Search'
 
 const Flights = () => {
+  const [origin, setOrigin] = useState('')
+  const [destination, setDestination] = useState('')
+
   return (
     <div className="Flights">
       <h1>Search for flights</h1>
-      <input type="text" placeholder="Where from?" className="input-origin"/>
-      <input type="text" placeholder="Where to?" className="input-destination"/>
+      <input
+        type="text"
+        className="input-origin"
+        placeholder="Where from?"
+        onChange={(e)=>setOrigin(e.target.value)}
+      />
+      <input
+        type="text"
+        className="input-destination"
+        placeholder="Where to?"
+        onChange={(e)=>setDestination(e.target.value)}
+      />
       <button className="search-button"><Search />Search</button>
     </div>
   )
