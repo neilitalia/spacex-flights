daacef0a3ba06305fb5de61a3bfa7d7e99dc68de
Neil Italia <neilitalia1998@gmail.com>
2021-08-31 23:37:22 -0500

    :sparkles: Search for flights

--- client/src/pages/Flights.jsx
+++ client/src/pages/Flights.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import './Flights.css'
+import Search from '../icons/Search'
 
 const Flights = () => {
   return (
-    <div>
-      <h1>Flights here</h1>
+    <div className="Flights">
+      <h1>Search for flights</h1>
+      <input type="text" placeholder="Where from?" className="input-origin"/>
+      <input type="text" placeholder="Where to?" className="input-destination"/>
+      <button className="search-button"><Search />Search</button>
     </div>
   )
 }
