a362253bf03cdda175acefa15a475c4a8245956a
Neil Italia <neilitalia1998@gmail.com>
2021-09-02 13:56:06 -0500

    :sparkles: Make icon colors dynamic

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = (props) => {
   return (
     <header className="Header">
       <h1>One giant leap for mankind.</h1>
-      <p>Our next step to becoming multi-planet species: Go where you want to go. Explore the known and unknown. Find cities terraformed to your taste. A new variety of civilizations await your arrival. </p>
+      <p>Our next step to becoming multi-planetary species: Go where you want to go. Explore the known and unknown. Find cities terraformed to your taste. A new variety of civilizations await your arrival. </p>
       <button className="earliest-flight" onClick={props.scrollToEnd}>Join Earliest Flight</button>
       <NavLink to='/flights'>
         Explore Destinations
--- client/src/icons/CheckMark.jsx
+++ client/src/icons/CheckMark.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const CheckMark = () => {
+const CheckMark = (props) => {
   return (
-    <svg fill="#ffffff" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 32 32" width="32px" height="32px"><path d="M 28.28125 6.28125 L 11 23.5625 L 3.71875 16.28125 L 2.28125 17.71875 L 10.28125 25.71875 L 11 26.40625 L 11.71875 25.71875 L 29.71875 7.71875 Z"/></svg>
+    <svg fill={props.fill || '#ffffff'} xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 32 32" width="32px" height="32px"><path d="M 28.28125 6.28125 L 11 23.5625 L 3.71875 16.28125 L 2.28125 17.71875 L 10.28125 25.71875 L 11 26.40625 L 11.71875 25.71875 L 29.71875 7.71875 Z"/></svg>
   )
 }
 
