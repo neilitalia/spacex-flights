4ea21ea6bc20d5afbf7606357747d2d33de262b2
Neil Italia <neilitalia1998@gmail.com>
2021-09-02 13:01:38 -0500

    :sparkles: Add background image

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
 import './Header.css'
 
 const Header = () => {
@@ -7,7 +8,9 @@ const Header = () => {
       <h1>One giant leap for mankind.</h1>
       <p>Our next step to becoming multi-planet species: Go where you want to go. Explore the known and unknown. Find cities terraformed to your taste. A new variety of civilizations await your arrival. </p>
       <button className="earliest-flight">Join Earliest Flight</button>
-      <button className="all-flights">Explore Destinations</button>
+      <NavLink to='/flights'>
+        Explore Destinations
+      </NavLink>
     </header>
   )
 }
