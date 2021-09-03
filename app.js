e1753ac2e6f51ad59172bb62e8e6ea1977217c82
Neil Italia <neilitalia1998@gmail.com>
2021-09-03 12:26:11 -0500

    :bug: Fix deployed GET request bug

--- app.js
+++ app.js
@@ -15,14 +15,14 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(logger('dev'))
 
+app.use('/api', AppRouter)
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')))
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(`${__dirname}/client/build/index.html`))
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
   })
 }
-
-app.use('/api', AppRouter)
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
